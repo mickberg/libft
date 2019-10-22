0ab06785b0888ad53a43c3703921bf21391f5f3c
Mikael Berglund <mberglun@student.hive.fi>
2019-10-22 13:54:07 +0300

    use library funcitons for tests

--- tests/ft_itoa.c
+++ tests/ft_itoa.c
@@ -6,12 +6,11 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 12:57:34 by mberglun          #+#    #+#             */
-/*   Updated: 2019/10/22 13:43:03 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/10/22 13:53:52 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "testlib.h"
-#include <stdlib.h>
 
 void	test_ft_itoa(void)
 {
@@ -22,25 +21,25 @@ void	test_ft_itoa(void)
 	int n4 = -123;
 
 	failed = 0;
-	if (atoi( ft_itoa(n1)) != n1)
+	if (ft_strcmp(ft_itoa(n1), "-2147483648") != 0)
 	{
 		ft_printfail("[ft_itoa] test 1");
 		ft_printdiff_str("-2147483648", ft_itoa(n1));
 		++failed;
 	}
-	if (atoi( ft_itoa(n2)) != n2)
+	if (ft_strcmp(ft_itoa(n2), "0") != 0)
 	{
 		ft_printfail("[ft_itoa] test 2");
 		ft_printdiff_str("0", ft_itoa(n2));
 		++failed;
 	}
-	if (atoi( ft_itoa(n3)) != n3)
+	if (ft_strcmp(ft_itoa(n3), "123456") != 0)
 	{
 		ft_printfail("[ft_itoa] test 3");
 		ft_printdiff_str("123456", ft_itoa(n3));
 		++failed;
 	}
-	if (atoi( ft_itoa(n4)) != n4)
+	if (ft_strcmp(ft_itoa(n4), "-123") != 0)
 	{
 		ft_printfail("[ft_itoa] test 4");
 		ft_printdiff_str("-123", ft_itoa(n4));
