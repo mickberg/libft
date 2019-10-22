786b3be1656ece23e1c2efb28c0453853d3e30e8
Mikael Berglund <mberglun@student.hive.fi>
2019-10-22 13:49:31 +0300

    replace printf with ft_itoa in test helpers

--- tests/helpers.c
+++ tests/helpers.c
@@ -9,7 +9,6 @@
 #include <unistd.h>
 #include <string.h>
 #include "testlib.h"
-#include <stdio.h>
 
 void	ft_putstr(char *str)
 {
@@ -42,5 +41,9 @@ void	ft_printdiff_str(char *s1, char *s2)
 
 void	ft_printdiff_int(int n1, int n2)
 {
-	printf(" - Expected: [%d] got [%d].\n", n1, n2);
+	ft_putstr(" - Expected: [");
+	ft_putstr(ft_itoa(n1));
+	ft_putstr("] got [");
+	ft_putstr(ft_itoa(n2));
+	ft_putstr("]\n");
 }
