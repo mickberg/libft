266e7ac3b52f504004dd2fedb89865aae936cb4b
Mikael Berglund <mberglun@student.hive.fi>
2019-10-25 19:04:04 +0300

    sqash this

--- tests/ft_memset.c
+++ tests/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 18:53:38 by mberglun          #+#    #+#             */
-/*   Updated: 2019/10/25 18:56:30 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/10/25 19:02:44 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ int	test_ft_memset(void)
 	int		failed = 0;
 	char	*name = "ft_memset";
 
-	char	*str1[10];
+	char	str1[10];
+	char	str2[20];
+	char	str3[5];
 
 	failed += test_str(memset(str1, 68, 9), ft_memset(str1, 68, 9), name, "normal");
+	failed += test_str(memset(str2, 68, 0), ft_memset(str2, 68, 0), name, "nofill");
+	failed += test_str(memset(str3, '\0', 5), ft_memset(str3, '\0', 5), name, "fill all");
+
 	return (failed);
 }
