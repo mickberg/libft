f17cae6a6a7054b1fa7c807278cf230029209b6c
mickberg <mikael.berglund2@gmail.com>
2019-10-28 20:37:04 +0200

    fix ft_atoi leading 0, and rnage bugs

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,7 +2,7 @@
  * File              : ft_atoi.c
  * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
  * Date              : 22.10.2019
- * Last Modified Date: 22.10.2019
+ * Last Modified Date: 28.10.2019
  * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
  */
 
@@ -13,7 +13,7 @@ int	ft_atoi(char *str)
 
 	sign = 1;
 	num = 0;
-	while ((*str >= 9 && *str <= 13) || *str == 32)
+	while ((*str >= 9 && *str <= 13) || *str == 32 || *str == 48)
 		++str;
 	if (*str == '+')
 		++str;
@@ -24,7 +24,7 @@ int	ft_atoi(char *str)
 	}
 	while (*str != '\0')
 	{
-		if (*str >= 57 || *str <= 48)
+		if (*str > 57 || *str < 48)
 			break ;
 		num = (num * 10) + *str - 48;
 		++str;
