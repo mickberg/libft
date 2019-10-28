cb3c4e92146580bc7fcadaba8ee910cfe72c201b
mickberg <mikael.berglund2@gmail.com>
2019-10-28 20:52:03 +0200

    ft_strnstr fix bug where search would continue after max len if match was found before

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -2,7 +2,7 @@
  * File              : ft_strnstr.c
  * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
  * Date              : 24.10.2019
- * Last Modified Date: 24.10.2019
+ * Last Modified Date: 28.10.2019
  * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
  */
 
@@ -12,6 +12,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	const char	*str;
 	const char	*need;
+	size_t		sublen;
 
 	if (*needle == '\0')
 		return ((char*)haystack);
@@ -20,7 +21,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		str = haystack;
 		need = needle;
-		while (*str++ == *need++)
+		sublen = len;
+		while (*str++ == *need++ && --sublen)
 		{
 			if (*need == '\0')
 				return ((char*)haystack);
