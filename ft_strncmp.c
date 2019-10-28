1c32bd3a6186b5db416e4236ecd62c6bfdb1f071
mickberg <mikael.berglund2@gmail.com>
2019-10-28 21:11:25 +0200

    fix bug for 0 length and exact len match

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,19 +2,23 @@
  * File              : ft_strncmp.c
  * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
  * Date              : 22.10.2019
- * Last Modified Date: 24.10.2019
+ * Last Modified Date: 28.10.2019
  * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
  */
 #include "libft.h"
 
 int		ft_strncmp(char *s1, char *s2, size_t n)
 {
-	while (*s1 == *s2 && n--)
+	while (n && *s1 == *s2)
 	{
 		if (*s1 == '\0')
 			return (0);
 		++s1;
 		++s2;
+		--n;
 	}
-	return ((unsigned char)*s1 - ((unsigned char)*s2));
+	if (n == 0)
+		return (0);
+	else
+		return ((unsigned char)*s1 - ((unsigned char)*s2));
 }
