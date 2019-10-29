7eb88d04abb29e8726e8c92501c84cce0b5cec03
mickberg <mikael.berglund2@gmail.com>
2019-10-29 13:18:09 +0200

    ft_memdel

---
+++ ft_memdel.c
@@ -0,0 +1,15 @@
+/**
+ * File              : ft_memdel.c
+ * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
+ * Date              : 29.10.2019
+ * Last Modified Date: 29.10.2019
+ * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
+ */
+
+#include "libft.h"
+
+void	ft_memdel(void **ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
+}
--- libft.h
+++ libft.h
@@ -45,5 +45,6 @@ void				*ft_memchr(const void *src, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void				*ft_memalloc(size_t size);
+void				ft_memdel(void **ptr);
 
 #endif
