4b569a5112bab378593981a26b953efca59e1656
mickberg <mikael.berglund2@gmail.com>
2019-10-29 12:55:53 +0200

    ft_memalloc

---
+++ ft_memalloc.c
@@ -0,0 +1,20 @@
+/**
+ * File              : ft_memalloc.c
+ * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
+ * Date              : 29.10.2019
+ * Last Modified Date: 29.10.2019
+ * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
+ */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = malloc(size);
+	if (!mem)
+		return (NULL);
+	ft_memset(mem, 0, size);
+	return (mem);
+}
