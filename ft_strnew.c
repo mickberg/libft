7234c8be92f4097f7bd6dcfd88da9aaf8782d431
mickberg <mikael.berglund2@gmail.com>
2019-10-29 17:56:13 +0200

    ft_strnew fix

--- ft_strnew.c
+++ ft_strnew.c
@@ -1,10 +1,14 @@
-/**
- * File              : ft_strnew.c
- * Author            : Mikael Berglund <mikael.berglund2@gmail.com>
- * Date              : 29.10.2019
- * Last Modified Date: 29.10.2019
- * Last Modified By  : Mikael Berglund <mikael.berglund2@gmail.com>
- */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mikaelberglund <marvin@42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 17:53:24 by mikaelber         #+#    #+#             */
+/*   Updated: 2019/10/29 17:55:28 by mikaelber        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -12,11 +16,9 @@ char	*ft_strnew(size_t size)
 {
 	char	*str;
 
-	if (size == 0)
-		return (NULL);
-	str = (char*)malloc(sizeof(char) * size);
+	str = (char*)malloc(sizeof(char) * size + 1);
 	if (!str)
 		return (NULL);
-	ft_memset(str, '\0', size);
+	ft_memset(str, '\0', size + 1);
 	return (str);
 }
