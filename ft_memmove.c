cb59d86e5a158c908fc6dfc9ba548ee8f97cfbf7
Mikael Berglund <mberglun@student.hive.fi>
2019-11-05 18:03:10 +0200

    new ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:07:01 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/05 17:45:18 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/05 18:01:58 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,21 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	char	*dest;
 	char	*source;
-	char	*buffer;
 	size_t	i;
 
-	buffer = (char*)malloc(sizeof(char) * n);
 	dest = (char*)dst;
 	source = (char*)src;
-	i = 0;
-	while (i < n)
+	if (dest > source)
+		while (n--)
+			dest[n] = source[n];
+	else
 	{
-		buffer[i] = source[i];
-		++i;
+		i = 0;
+		while (i < n)
+		{
+			dest[i] = source[i];
+			++i;
+		}
 	}
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = buffer[i];
-		++i;
-	}
-	free(buffer);
-	buffer = NULL;
 	return (dst);
 }
