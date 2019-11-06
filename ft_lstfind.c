fc9484b0e3498e7fd5b0da1cc0581fbe72b61857
Mikael Berglund <mberglun@student.hive.fi>
2019-11-06 14:16:09 +0200

    norm lstfind

--- ft_lstfind.c
+++ ft_lstfind.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstfind.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/06 14:15:35 by mberglun          #+#    #+#             */
+/*   Updated: 2019/11/06 14:15:52 by mberglun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstfind(t_list *lst, int (*f)(void *, size_t))
@@ -5,7 +17,7 @@ t_list	*ft_lstfind(t_list *lst, int (*f)(void *, size_t))
 	if (!lst)
 		return (NULL);
 	if (f(lst->content, lst->content_size) != 0)
-		return lst;
+		return (lst);
 	else
 		return (ft_lstfind(lst->next, f));
 }
