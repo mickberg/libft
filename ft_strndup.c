0306aa7c6b1244e416feacc881b6a0fd5e3849c7
Mikael Berglund <mberglun@student.hive.fi>
2019-11-06 14:15:08 +0200

    fix typo

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,26 +6,26 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:10:17 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/01 20:10:18 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/06 14:00:04 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strndup(const char *str, size_t n)
+char	*ft_strndup(const char *str, size_t len)
 {
 	char	*dup;
 	char	*d;
 	size_t	strlen;
 
 	strlen = ft_strlen((char*)str);
-	if (strlen < n)
-		n = strlen;
-	dup = (char*)malloc(sizeof(char) * (n + 1));
+	if (strlen < len)
+		len = strlen;
+	dup = (char*)malloc(sizeof(char) * (len + 1));
 	if (!dup)
 		return (NULL);
 	d = dup;
-	while (*str != '\0' && n--)
+	while (*str != '\0' && len--)
 		*d++ = (char)*str++;
 	*d = '\0';
 	return (dup);
