77aa24434781a5962dfcfc26890aa2fd12bb21db
mickberg <mikael.berglund2@gmail.com>
2019-11-08 23:00:56 +0200

    ft_strncpy improved

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,18 +6,18 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:10:12 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/01 20:10:14 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/08 22:50:55 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	char	*d;
 
 	d = dst;
-	while (len-- > 0)
+	while (len--)
 	{
 		*d = *src;
 		if (*src != '\0')
