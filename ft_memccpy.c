197e89326a8bef0fb5ab9ebb54316f8f34795f38
mickberg <mikael.berglund2@gmail.com>
2019-11-08 20:59:01 +0200

    ft_memccpy improved

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,24 +6,25 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:06:10 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/06 19:04:16 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/08 19:18:46 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	char		*d;
-	const char	*s;
+	unsigned char		cc;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	d = dst;
-	s = src;
+	cc = (unsigned char)c;
+	d = (unsigned char*)dst;
+	s = (const unsigned char*)src;
 	while (n--)
 	{
 		*d++ = *s++;
-		if ((unsigned char)*(s - 1) == (unsigned char)c)
+		if (*(s - 1) == cc)
 			return (d);
 	}
 	return (NULL);
