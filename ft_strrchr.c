6ac9e23a8e185100079805d49c3eb4db175cf3b5
mickberg <mikael.berglund2@gmail.com>
2019-11-09 20:27:54 +0200

    ft_strrchr clean

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,26 +6,24 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 16:55:19 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/02 16:55:24 by mberglun         ###   ########.fr       */
+/*   Updated: 2019/11/09 20:23:21 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
-	const char *i;
 	const char *match;
 
 	match = NULL;
-	i = s;
-	while (*i != '\0')
+	while (*s != '\0')
 	{
-		if ((int)*i == c)
-			match = i;
-		++i;
+		if ((int)*s == c)
+			match = s;
+		++s;
 	}
 	if (c == '\0')
-		return ((char*)i);
+		return ((char*)s);
 	return ((char*)match);
 }
