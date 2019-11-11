ea2a5c81a51cd033e672d6da3f0040edc7ece2b2
Mikael Berglund <mberglun@student.hive.fi>
2019-11-11 16:36:35 +0200

    fix ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mberglun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:06:18 by mberglun          #+#    #+#             */
-/*   Updated: 2019/11/08 21:22:08 by mikaelber        ###   ########.fr       */
+/*   Updated: 2019/11/11 16:35:46 by mberglun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	*ft_memchr(const void *src, int c, size_t n)
 {
 	unsigned char	cc;
-	char			*source;
+	unsigned char	*source;
 
 	cc = (unsigned char)c;
-	source = (char*)src;
+	source = (unsigned char*)src;
 	while (n--)
 	{
-		if (*source == c)
+		if (*source == cc)
 			return ((void*)source);
 		++source;
 	}
